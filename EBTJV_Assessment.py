53a196d21d33e826255826f51ad2dfb015fdc116
Jason <jcoombs@cns.umass.edu>
2015-02-11 16:35:41 -0500

    Added in ability to overwrite troutless samples within a user specified time period, and added option to classify catchments based on salmonid species present in samples in the classification year in a catchment

--- EBTJV_Assessment.py
+++ EBTJV_Assessment.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 #Gets EBTJV assessment code based on salmonid species present
-def get_code(bkt, bnt, rbt):
+def get_code(bkt, bnt, rbt, bktStock):
 
 	if useStock == True:
 		if bkt >= 1 and bnt >= 1 and rbt >= 1:
@@ -13,7 +13,7 @@ def get_code(bkt, bnt, rbt):
 		elif bkt >= 1 and bnt >= 1:
 			return "1.2"
 		elif bkt >= 1:
-			if bktRow[7] >= 1:
+			if bktStock >= 1:
 				return "1.5"
 			else:
 				return "1.1"
@@ -23,7 +23,7 @@ def get_code(bkt, bnt, rbt):
 			return "0.3"
 		elif bnt >= 1:
 			return "0.2"
-		elif bktRow[7] >= 1:
+		elif bktStock >= 1:
 			return "0.5"
 		else:
 			return "0"
@@ -45,6 +45,45 @@ def get_code(bkt, bnt, rbt):
 		else:
 			return "0"
 
+
+#Compare species occurrence for two samples in the same year in the same catchment
+def findSpecies(newCode, oldCode):
+	status = "unchanged"
+	bkt = 0
+	bktStock = 0
+	bnt = 0
+	rbt = 0
+
+	if oldCode[0] == "1":
+		bkt = 1
+	if oldCode[0:3] == "0.5":
+		bktStock = 1
+	if oldCode[0:3] == "0.2" or oldCode[0:3] == "0.4" or oldCode[0:3] == "1.2" or oldCode[0:3] == "1.4":
+		bnt = 1
+	if oldCode[0:3] == "0.3" or oldCode[0:3] == "0.4" or oldCode[0:3] == "1.3" or oldCode[0:3] == "1.4":
+		rbt = 1
+
+	if newCode[0] == "1":
+		if bkt == 0:
+			bkt = 1
+			status = "changed"
+	if newCode[0:3] == "0.5":
+		if bktStock == 0:
+			bktStock = 1
+			status = "changed"
+	if newCode[0:3] == "0.2" or newCode[0:3] == "0.4" or newCode[0:3] == "1.2" or newCode[0:3] == "1.4":
+		if bnt == 0:
+			bnt = 1
+			status = "changed"
+	if newCode[0:3] == "0.3" or newCode[0:3] == "0.4" or newCode[0:3] == "1.3" or newCode[0:3] == "1.4":
+		if rbt == 0:
+			rbt = 1
+			status = "changed"
+	
+	arcpy.AddMessage("{0}, {1}, {2}, {3}".format(status, bkt, bnt, rbt))
+	return status, bkt, bnt, rbt, bktStock
+
+
 #compares dam and sample point locations to determine if sample is upstream or downstream of dam
 def dam_samp(damInd):
 	damFDRNull = str(arcpy.GetCellValue_management(fdrNull, "{0} {1}".format(damArray[damInd][1], damArray[damInd][2]), "1").getOutput(0))
@@ -60,6 +99,7 @@ def dam_samp(damInd):
 	sampFDR = arcpy.GetCellValue_management(fdr, "{0} {1}".format(tmpSamp[0], tmpSamp[1]), "1")
 	sampLoc = getLoc(tmpDam, damFDR, tmpSamp, sampFDR)
 	return sampLoc
+
 
 #Determines if sample location is above or below dam location
 def getLoc(tmpDam, damFDR, tmpSamp, sampFDR):
@@ -106,6 +146,7 @@ def getLoc(tmpDam, damFDR, tmpSamp, sampFDR):
 	else:
 		return "Below"
 
+
 #Moves dam and sample points to nearest stream using fdr (flow direction) raster layer
 def getNoData(X,Y, tmpVal):
 	while tmpVal != "NoData":
@@ -134,6 +175,7 @@ def getNoData(X,Y, tmpVal):
 		tmpVal = str(arcpy.GetCellValue_management(fdrNull, "{0} {1}".format(X, Y), "1").getOutput(0))
 	return [X, Y]
 
+
 #gets PlusFlowlineVAA records matching catchment polygon feature id
 def get_PFVAA(featureID, catchCnt, tmpLength):
 	try:
@@ -141,6 +183,7 @@ def get_PFVAA(featureID, catchCnt, tmpLength):
 		get_PFlow(vaaArray[vaaIndex][1], catchCnt, vaaArray[vaaIndex][2], featureID, tmpLength)
 	except ValueError:
 		arcpy.AddMessage("Error indexing FlowlineVAA comID {0}".format(featureID))
+
 
 #gets PlusFlow records matching the PFVAA NodeNumber
 def get_PFlow(NodeNum, catchCnt, strOrd, oldFeat, tmpLength):
@@ -156,6 +199,7 @@ def get_PFlow(NodeNum, catchCnt, strOrd, oldFeat, tmpLength):
 				break
 	except ValueError:
 		arcpy.AddMessage("Error indexing Flowline Node Number {0}".format(NodeNum))
+
 
 #gets catchment polygons for catchments upstream of sample point
 def get_catchPoly(featureID, catchCnt, strOrd, NodeNum, fromComID, tmpLength):
@@ -214,6 +258,7 @@ def get_catchPoly(featureID, catchCnt, strOrd, NodeNum, fromComID, tmpLength):
 		
 			if cpArray[cpInd][6] == "No":
 				get_PFVAA(cpArray[cpInd][1], catchCnt, tmpLength)
+
 
 #searches for feature ID when NHD 'FromComID' doesn't exist
 def find_comID(fromComID, featureID, NodeNum):
@@ -278,6 +323,7 @@ def find_comID(fromComID, featureID, NodeNum):
 			streamArray[streamIndex][2] = 0
 			return 0
 
+
 #determines if dam is present in catchment polygon based on featureID
 def get_dam(comID):
 	try:
@@ -286,6 +332,7 @@ def get_dam(comID):
 	except ValueError:
 		return "No", 0
 
+
 #determines stream order based on featureID
 def get_order(comID):
 	try:
@@ -293,6 +340,7 @@ def get_order(comID):
 		return vaaArray[vaaIndex][2]
 	except ValueError:
 		return 0
+
 
 #Writes cpArray to catchPoly
 def dump_cpArray():
@@ -304,32 +352,35 @@ def dump_cpArray():
 			cpRecords.updateRow(cpRow)
 		i += 1
 
+
 #***** Beginning of algorithm*****
 sys.setrecursionlimit(10000)
 
 bktPoints = arcpy.GetParameterAsText(0)
 useStock = arcpy.GetParameter(1)
 useAbsence = arcpy.GetParameter(2)
-catchRast = arcpy.GetParameterAsText(3)
-catchPoly = arcpy.GetParameterAsText(4)
-streams = arcpy.GetParameterAsText(5)
-fdr = arcpy.GetParameterAsText(6)
-fdrNull = arcpy.GetParameterAsText(7)
-dams = arcpy.GetParameterAsText(8)
-plusFlowVAA = arcpy.GetParameterAsText(9)
-plusFlow = arcpy.GetParameterAsText(10)
-sampYear = long(arcpy.GetParameterAsText(11))
-sampDiff = long(arcpy.GetParameterAsText(12))
-usepntDis = arcpy.GetParameter(13)
-pntDis = long(arcpy.GetParameterAsText(14))
-usestrOrd = arcpy.GetParameter(15)
-strOrdBkt = long(arcpy.GetParameterAsText(16))
-strOrdInv = long(arcpy.GetParameterAsText(17))
-usestrDis = arcpy.GetParameter(18)
-strDis = float(arcpy.GetParameterAsText(19))
-usedamDis = arcpy.GetParameter(20)
-damDis = float(arcpy.GetParameterAsText(21))
-addDel = arcpy.GetParameter(22)
+absYear = long(arcpy.GetParameterAsText(3))
+catchRast = arcpy.GetParameterAsText(4)
+catchPoly = arcpy.GetParameterAsText(5)
+streams = arcpy.GetParameterAsText(6)
+fdr = arcpy.GetParameterAsText(7)
+fdrNull = arcpy.GetParameterAsText(8)
+dams = arcpy.GetParameterAsText(9)
+plusFlowVAA = arcpy.GetParameterAsText(10)
+plusFlow = arcpy.GetParameterAsText(11)
+sampYear = long(arcpy.GetParameterAsText(12))
+sampDiff = long(arcpy.GetParameterAsText(13))
+usepntDis = arcpy.GetParameter(14)
+pntDis = long(arcpy.GetParameterAsText(15))
+usestrOrd = arcpy.GetParameter(16)
+strOrdBkt = long(arcpy.GetParameterAsText(17))
+strOrdInv = long(arcpy.GetParameterAsText(18))
+usestrDis = arcpy.GetParameter(19)
+strDis = float(arcpy.GetParameterAsText(20))
+usedamDis = arcpy.GetParameter(21)
+damDis = float(arcpy.GetParameterAsText(22))
+addDel = arcpy.GetParameter(23)
+classType = arcpy.GetParameter(24)
 
 tmpDescribe = arcpy.Describe(catchPoly)
 tmpPath = "{0}".format(tmpDescribe.dataElement.catalogPath)
@@ -347,6 +398,8 @@ tmpParam = open("{0}/Catchment Parameter Settings.txt".format(outPath), "w")
 tmpParam.write("Sample Location Point Layer: {0}\n".format(bktPoints))
 tmpParam.write("Use stocking information: {0}\n".format(useStock))
 tmpParam.write("Use 'Trout Absent' sampling points: {0}\n".format(useAbsence))
+if useAbsence == True:
+	tmpParam.write("Absent since (year): {0}\n".format(absYear))
 tmpParam.write("Catchment Raster Layer: {0}\n".format(catchRast))
 tmpParam.write("Catchment Feature Layer: {0}\n".format(catchPoly))
 tmpParam.write("Flowline Feature Layer: {0}\n".format(streams))
@@ -371,6 +424,7 @@ tmpParam.write("Use Max Barrier Distance: {0}\n".format(usedamDis))
 if usedamDis == True:
 	tmpParam.write("Max Barrier Point Distance (m): {0}\n".format(damDis))
 tmpParam.write("Delete and Re-Add Fields: {0}\n".format(addDel))
+tmpParam.write("Use only most recent sample: {0}\n".format(classType))
 tmpParam.close()
 
 #Get spatial reference for catchments raster layer to use with cursors
@@ -389,7 +443,7 @@ if addDel == True:
 	arcpy.AddField_management(catchPoly, "Cum_Length", "DOUBLE")
 	arcpy.AddField_management(catchPoly, "Samp_Year", "LONG")
 	arcpy.AddField_management(catchPoly, "Samp_Dist", "DOUBLE")
-	arcpy.AddField_management(catchPoly, "Samp_OID", "LONG")
+	arcpy.AddField_management(catchPoly, "Samp_OID", "TEXT")
 	arcpy.AddField_management(catchPoly, "Dam", "TEXT")
 	arcpy.AddField_management(catchPoly, "Samp_Loc", "TEXT")
 	arcpy.AddField_management(catchPoly, "Str_Order", "LONG")
@@ -469,6 +523,8 @@ for pfRow in sorted(pfRecords):
 
 bktRecords = arcpy.da.SearchCursor(bktPoints, bktFields, "", CRSR.spatialReference)
 i = 0
+absBi = 0
+classBi = 0
 
 for bktRow in sorted(bktRecords, reverse=True):
 	#Get sampling occasion year
@@ -479,7 +535,12 @@ for bktRow in sorted(bktRecords, reverse=True):
 	arcpy.AddMessage("sample: {0}, Date: {1}".format(i, bktRow[0]))
 	
 	#Get assessment code
-	tmpCode = get_code(bktRow[3], bktRow[4], bktRow[5])
+	if useStock == True:
+		tmpCode = get_code(bktRow[3], bktRow[4], bktRow[5], bktRow[7])
+		
+	else:
+		tmpCode = get_code(bktRow[3], bktRow[4], bktRow[5], 0)
+	
 	tmpBi = 1
 	if useAbsence == False:
 		if tmpCode == "0":
@@ -497,7 +558,7 @@ for bktRow in sorted(bktRecords, reverse=True):
 		if sampYear - tmpYear > 10:
 			tmpCode += "P"
 
-		sampOID = bktRow[6]
+		sampOID = str(bktRow[6])
 
 		#Get catchRast value of sampling occasion
 		try:
@@ -525,8 +586,29 @@ for bktRow in sorted(bktRecords, reverse=True):
 						damHere = "No"
 				#arcpy.AddMessage("tmpdamDist = {0}, damDis = {1}, damHere = {2}".format(tmpdamDist, damDis, damHere))
 
-			if ((tmpYear + sampDiff) >= cpArray[cpIndex][4] and cpArray[cpIndex][3] > 1) or cpArray[cpIndex][3] == 0:
+			#Accomodate for trout present samples occurring prior to a trout absent sample but after the absence year and greater than the overwrite period
+			if useAbsence == True:
+				absBi = 0
+				if ((cpArray[cpIndex][2] == "0" or cpArray[cpIndex][2] == "0P") and tmpCode <> "0" and tmpCode <> "0P"):
+					if tmpYear >= absYear and (tmpYear + sampDiff) >= cpArray[cpIndex][4]:
+						absBi = 1
+
+			#Determine if samples should be classified based on most recent sample or most recent year
+			if classType == False:
+				classBi = 0
+				if tmpYear == cpArray[cpIndex][4]:
+					if tmpCode != cpArray[cpIndex][2] and tmpCode != "0" and tmpCode != "0P":
+						status, bkt, bnt, rbt, bktStock = findSpecies(tmpCode, cpArray[cpIndex][2])
+						if status == "changed":
+							classBi = 1
+							tmpCode = get_code(bkt, bnt, rbt, bktStock)
+							sampOID = "{0}, {1}".format(cpArray[cpIndex][10], sampOID)
+
+			if ((tmpYear + sampDiff) >= cpArray[cpIndex][4] and cpArray[cpIndex][3] > 1) or cpArray[cpIndex][3] == 0 or absBi == 1 or classBi == 1:
 				if fCode != 56600: #Coastline
+					if absBi == 1:
+						cpArray[cpIndex][8] = "{0} = {1}; {2}".format(cpArray[cpIndex][4], cpArray[cpIndex][2], cpArray[cpIndex][8])
+
 					cpArray[cpIndex][2] = tmpCode
 					cpArray[cpIndex][3] = catchCnt
 					cpArray[cpIndex][4] = tmpYear
@@ -570,7 +652,7 @@ for bktRow in sorted(bktRecords, reverse=True):
 					if sampLoc != "Below" and tmpCode != "0" and tmpCode != "0P" and tmpCode != "0.5" and tmpCode != "0.5P" and tmpFilter == 0:
 						get_PFVAA(cpArray[cpIndex][1], catchCnt, tmpLength)
 			else:
-				cpArray[cpIndex][8] += "Sampled in " + str(tmpYear) + " and had an assessment code of " + tmpCode + ". "
+				cpArray[cpIndex][8] += "{0} = {1}; ".format(tmpYear, tmpCode)
 		except ValueError:
 			arcpy.AddMessage("{0}".format(gridCode))
 			arcpy.AddMessage("No Catchment for point at {0}, {1}".format(bktRow[1], bktRow[2]))
